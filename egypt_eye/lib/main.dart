8e5d4ab234aa5de89907146a2d63def801d3bdd8
unknown <mayar1908067@miuegypt.edu.eg>
2023-01-08 01:58:04 +0200

    changesedited

--- egypt_eye/lib/main.dart
+++ egypt_eye/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:egypt_eye/view/map.dart';
+import 'package:egypt_eye/view/HomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:egypt_eye/view/signin.dart';
 import 'package:egypt_eye/view/signup.dart';
